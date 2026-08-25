22cf367fcd70c8b6052598eaf9fc189cd0e363aa
khaleedk403 <khaleedk403@gmail.com>
2026-08-25 14:08:02 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,27 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 👇 KA ƘARA WANNAN
-app.get("/banks", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api.paystack.co/bank",
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
-        }
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: error.response?.data || error.message
-    });
-  }
-});
-
 app.post("/bank-name-enquiry", async (req, res) => {
   const { account_number, bank_code } = req.body;
 
@@ -65,3 +44,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
